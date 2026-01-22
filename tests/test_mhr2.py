a5732f1d1c06fe2697a25ed785b30b1c85e4cce3
EnshenZhu <winiecmz.zhu@gmail.com>
2026-01-22 17:14:55 -0500

    add the MHR tester

---
+++ tests/test_mhr2.py
@@ -0,0 +1,23 @@
+import torch
+from mhr.mhr import MHR
+
+def mhr_tester():
+    # Load MHR model (LOD 1, on CPU)
+    mhr_model = MHR.from_files(device=torch.device("cpu"), lod=1)
+
+    # Define parameters
+    batch_size = 2
+    identity_coeffs = 0.8 * torch.randn(batch_size, 45)      # Identity
+    model_parameters = 0.2 * (torch.rand(batch_size, 204) - 0.5)  # Pose
+    face_expr_coeffs = 0.3 * torch.randn(batch_size, 72)     # Facial expression
+
+    # Generate mesh vertices and skeleton information (joint orientation and positions).
+    vertices, skeleton_state = mhr_model(identity_coeffs, model_parameters, face_expr_coeffs)
+
+    return vertices, skeleton_state
+
+if __name__ == "__main__":
+    print("start")
+    this_vertices, this_skeleton_state=mhr_tester()
+    print(this_vertices)
+    print(this_skeleton_state)
