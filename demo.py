fdceedee12190eefe24e02cffc47a1fa0e73fdf4
EnshenZhu <winiecmz.zhu@gmail.com>
2026-01-23 16:08:02 -0500

    enable cuda execution and update

--- demo.py
+++ demo.py
@@ -19,29 +19,32 @@ import trimesh
 
 torch.manual_seed(0)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Running on {device}.")
+
 def _prepare_input_data(batch_size: int) -> torch.Tensor:
-    identity_coeffs = 0.8 * torch.randn(batch_size, 45).cpu()
-    model_parameters = 0.2 * (torch.rand(batch_size, 204) - 0.5).cpu()
-    face_expr_coeffs = 0.3 * torch.randn(batch_size, 72).cpu()
+    identity_coeffs = 0.8 * torch.randn(batch_size, 45).to(device)
+    model_parameters = 0.2 * (torch.rand(batch_size, 204) - 0.5).to(device)
+    face_expr_coeffs = 0.1 * torch.randn(batch_size, 72).to(device)
     return identity_coeffs, model_parameters, face_expr_coeffs
 
 def run():
-    mhr_model = MHR.from_files(device=torch.device("cpu"), lod=1)
+    mhr_model = MHR.from_files(device=device, lod=1)
     batch_size = 2
     identity_coeffs, model_parameters, face_expr_coeffs = _prepare_input_data(batch_size)
 
     with torch.no_grad():
         verts, skel_state = mhr_model(identity_coeffs, model_parameters, face_expr_coeffs)
 
-    mesh = trimesh.Trimesh(vertices=verts[0].numpy(), faces=mhr_model.character.mesh.faces, process=False)
+    mesh = trimesh.Trimesh(vertices=verts[0].cpu().numpy(), faces=mhr_model.character.mesh.faces, process=False)
     output_mesh_path = "./test.ply"
     mesh.export(output_mesh_path)
     print(f"Saved example MHR mesh to {output_mesh_path}")
 
 def compare_with_torchscript_model():
-    print("Comparing MHR model with TorchScripted model.")
-    scripted_model = torch.jit.load("./assets/mhr_model.pt")
-    mhr_model = MHR.from_files(device=torch.device("cpu"), lod=1)
+    print(f"Comparing MHR model with TorchScripted model on {device}.")
+    scripted_model = torch.jit.load("./assets/mhr_model.pt", map_location=device)
+    mhr_model = MHR.from_files(device=device, lod=1)
 
     batch_size = 128
     identity_coeffs, model_parameters, face_expr_coeffs = _prepare_input_data(batch_size)
